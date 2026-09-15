267d7f6d582e13bcedf42f41304a38ad2d66811c
Shane Winter <shanewin@gmail.com>
2026-09-15 15:00:50 -0400

    Turn the dial to two

--- src/server.ts
+++ src/server.ts
@@ -124,7 +124,7 @@ const detectionSummary = [`Next.js app: ${appRoot}`, resolution.note].filter(Boo
 
 const server = new McpServer({
   name: 'codebase-lens',
-  version: '0.1.0',
+  version: '0.2.0',
 })
 
 // Register each collected tool
